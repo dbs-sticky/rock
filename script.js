e7453267756bda09eddcb38488b7dba9c8524966
Mark Evans <69587742+dbs-sticky@users.noreply.github.com>
2025-01-04 15:37:39 +0000

    Function to generate random rock, paper, or scissors created

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello, world!');
+// return a random string of: rock, paper, or scissors
+
+function getComputerChoice() {
+const options = ["rock", "paper", "scissors"]; //Create the array of possible options
+const randomZeroToThree = Math.floor(Math.random()*3); // create a random number between 0 and 3
+console.log(options[randomZeroToThree]); // use the random number to access one of the 3 elements in the array
+return options[randomZeroToThree]; // log the result to the console
+}
+
+getComputerChoice();
