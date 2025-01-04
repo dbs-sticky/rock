0f98156f8f0940c4dd4a739820442c1ce5450866
Mark Evans <69587742+dbs-sticky@users.noreply.github.com>
2025-01-04 20:32:07 +0000

    First draft with round working

--- script.js
+++ script.js
@@ -6,26 +6,43 @@ let computerScore = 0;
 function getComputerChoice() {
 const options = ["rock", "paper", "scissors"]; //Create the array of possible options
 const randomZeroToThree = Math.floor(Math.random()*3); // create a random number between 0 and 3
-console.log(options[randomZeroToThree]); // log the result to the console
-return options[randomZeroToThree]; // use the random number to access one of the 3 elements in the array, and return the result
+const computerChoose = options[randomZeroToThree];
+
+console.log(`The computer selected: ${computerChoose}`); // log the result to the console
+return computerChoose; // use the random number to access one of the 3 elements in the array, and return the result
 }
 
 
 function getHumanChoice() {
-let humanChoice = prompt("enter an option i.e. rock, paper, or scissors"); //get the value from the user
-humanChoice = humanChoice.toLowerCase(); // convert user entry to lower case, so case becomes irrelevant
-humanChoice = humanChoice.trim(); // remove any surrounding spaces entered accidently e.g. by Copy/Paste etc.
+let humanInput = prompt("enter an option i.e. rock, paper, or scissors"); //get the value from the user
+humanInput = humanInput.toLowerCase(); // convert user entry to lower case, so case becomes irrelevant
+humanInput = humanInput.trim(); // remove any surrounding spaces entered accidently e.g. by Copy/Paste etc.
 
-    if (humanChoice == "rock" || humanChoice ==  "paper" || humanChoice ==  "scissors") { // check that the entry is one of the 3 valid options
-        console.log(`Thank you, you have entered ${humanChoice}`); // show the user that their entry was recorded
-        return humanChoice;
+    if (humanInput == "rock" || humanInput ==  "paper" || humanInput ==  "scissors") { // check that the entry is one of the 3 valid options
+        console.log(`You selected: ${humanInput}`); // show the user that their entry was recorded
+        return humanInput;
             } else {
             console.log("Not a valid entry, please try again"); // let the user know that their entry was not valid
         }
 };
 
 
+function playRound(humanChoice, computerChoice) {
 
-getComputerChoice();
+};
 
-getHumanChoice();
+const humanSelection = getHumanChoice(); // set human choice to a new variable
+const computerSelection = getComputerChoice(); //set computer choice to a new variable
+
+playRound(humanSelection, computerSelection); // pass new variables into main function
+
+if (humanSelection == computerSelection) {console.log("It was a draw");} else if
+(humanSelection == "rock" && computerSelection == "scissors") {humanScore = ++humanScore;} else if
+(humanSelection == "scissors" && computerSelection == "paper") {humanScore = ++humanScore;} else if
+(humanSelection == "paper" && computerSelection == "rock") {humanScore = ++humanScore;} else if
+(computerSelection == "rock" && humanSelection == "scissors") {computerScore = ++computerScore;} else if
+(computerSelection == "scissors" && humanSelection == "paper") {computerScore = ++computerScore;} else if
+(computerSelection == "paper" && humanSelection == "rock") {computerScore = ++computerScore;} else 
+console.log("Error");
+
+console.log(`Computer Score: ${computerScore} . Player Score: ${humanScore}`) 
