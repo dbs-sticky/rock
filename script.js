5d026ac4fb3e748997ddca3b0fcfa37dbbffb0d4
Mark Evans <69587742+dbs-sticky@users.noreply.github.com>
2025-01-04 15:56:59 +0000

    HumanChoice working

--- script.js
+++ script.js
@@ -3,8 +3,26 @@
 function getComputerChoice() {
 const options = ["rock", "paper", "scissors"]; //Create the array of possible options
 const randomZeroToThree = Math.floor(Math.random()*3); // create a random number between 0 and 3
-console.log(options[randomZeroToThree]); // use the random number to access one of the 3 elements in the array
-return options[randomZeroToThree]; // log the result to the console
+console.log(options[randomZeroToThree]); // log the result to the console
+return options[randomZeroToThree]; // use the random number to access one of the 3 elements in the array, and return the result
 }
 
+
+
+function getHumanChoice() {
+const humanChoice = prompt("enter an option i.e. rock, paper, or scissors");
+
+    if (humanChoice == "rock" || humanChoice ==  "paper" || humanChoice ==  "scissors") {
+        console.log(`Thank you, you have entered ${humanChoice}`);
+        return humanChoice;
+            } else {
+            console.log("Not a valid entry, please try again");
+        }
+
+    
+
+};
+
 getComputerChoice();
+
+getHumanChoice();
