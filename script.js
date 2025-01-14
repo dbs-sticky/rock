ceed8c1ea1c5cb7c82de884c24f763bb2d440748
Mark Evans <69587742+dbs-sticky@users.noreply.github.com>
2025-01-14 20:55:47 +0000

    Working version with UI

--- script.js
+++ script.js
@@ -2,64 +2,89 @@
 let humanScore = 0; // set starting human score to zero
 let computerScore = 0; // set starting computer score to zero
 
+const scoreBoard = document.querySelector("#scores");
+
 function getComputerChoice() { // get the computer choice
 const options = ["rock", "paper", "scissors"]; //Create the array of possible options
 const randomZeroToThree = Math.floor(Math.random()*3); // create a random integer between 0 and 3
 const computerChoose = options[randomZeroToThree]; // use the integer to pick an value from the array
+console.clear();
 console.log(`The computer selected: ${computerChoose}`); // log the result to the console
+
+//create a new paragraph, and append to scoreboard
+const para = document.createElement("p");
+const score = scoreBoard.appendChild(para);
+score.textContent =`The computer selected: ${computerChoose}`;
 return computerChoose; // return the result i.e. rock, paper, or scissors
 }
 
 
-function getHumanChoice() { // get the human choise
-let humanInput = prompt("enter an option i.e. rock, paper, or scissors"); //get the value from the user
-humanInput = humanInput.toLowerCase(); // convert user entry to lower case, so case becomes irrelevant
-humanInput = humanInput.trim(); // remove any surrounding spaces entered accidently e.g. by Copy/Paste etc.
+// function getHumanChoice() { // get the human choice
+// let humanInput = prompt("enter an option i.e. rock, paper, or scissors"); //get the value from the user
+// humanInput = humanInput.toLowerCase(); // convert user entry to lower case, so case becomes irrelevant
+// humanInput = humanInput.trim(); // remove any surrounding spaces entered accidently e.g. by Copy/Paste etc.
 
-    if (humanInput == "rock" || humanInput ==  "paper" || humanInput ==  "scissors") { // check that the entry is one of the 3 valid options
-        console.log(`You selected: ${humanInput}`); // show the user that their entry was recorded
-        return humanInput;
-            } else {
-            console.log("Not a valid entry, please try again"); // let the user know that their entry was not valid
-        }
-}
+//     if (humanInput == "rock" || humanInput ==  "paper" || humanInput ==  "scissors") { // check that the entry is one of the 3 valid options
+//         console.log(`You selected: ${humanInput}`); // show the user that their entry was recorded
+//         return humanInput;
+//             } else {
+//             console.log("Not a valid entry, please try again"); // let the user know that their entry was not valid
+//         }
+// }
 
 
 function playRound(humanSelect, computerSelect) { // function to play a single round, include rock, paper, scissors logic
+
     if (humanSelect === computerSelect) { // the case when it's a draw
-        console.log("It was a draw");
+        //create a new paragraph, and append to scoreboard
+        const para = document.createElement("p");
+        const score = scoreBoard.appendChild(para);
+        score.textContent =`You also selected ${humanSelect} It was a draw`;
     } else if ( // the 3 cases where the human wins
         (humanSelect === "rock" && computerSelect === "scissors") ||
         (humanSelect === "scissors" && computerSelect === "paper") ||
         (humanSelect === "paper" && computerSelect === "rock")
     ) {
         humanScore++; // iterate the human score
-        console.log("You win this round!");
+        const para = document.createElement("p");
+        const score = scoreBoard.appendChild(para);
+        score.textContent =`You selected ${humanSelect} You win!`;
     } else if ( // the case where the computer wins
         (computerSelect === "rock" && humanSelect === "scissors") ||
         (computerSelect === "scissors" && humanSelect === "paper") ||
         (computerSelect === "paper" && humanSelect === "rock")
     ) {
         computerScore++; // iterate the computer score
-        console.log("Computer wins this round!");
+        const para = document.createElement("p");
+        const score = scoreBoard.appendChild(para);
+        score.textContent =`You selected ${humanSelect} You lose!`;
     } else {
         console.log("Error"); // if none of the cases above are met, log "error" to the console
     };
 }
 
-function playGame() {
-    for (let gameNumber = 1; gameNumber <= 5; gameNumber++){ //iterate the round 5 times
-        const humanSelection = getHumanChoice(); //get the human choice
-        const computerSelection = getComputerChoice(); // get the computer
-        playRound(humanSelection, computerSelection); // play a round
-        console.log(`Round: ${gameNumber}. Computer Score: ${computerScore} . Player Score: ${humanScore}`); // log the cumulative score
-    } 
-    console.log("Game over!"); // let the user know the game has ended
-    if (humanScore == computerScore) { // if it was a draw…
-        console.log("It was a draw"); // … let the user know
-    } else { // else tell them who the winner was
-        humanScore > computerScore ? console.log("You win 😊") : console.log("Computer wins 😢")
-    }
-}
+// add event listeners to the 3 buttons i.e. rock, paper, and scissors
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener("click", () => playRound(button.id, getComputerChoice()));
+});
 
-playGame() // play the game
+
+addEventListener
+
+// function playGame() {
+//     for (let gameNumber = 1; gameNumber <= 5; gameNumber++){ //iterate the round 5 times
+//         const humanSelection = getHumanChoice(); //get the human choice
+//         const computerSelection = getComputerChoice(); // get the computer
+//         playRound(humanSelection, computerSelection); // play a round
+//         console.log(`Round: ${gameNumber}. Computer Score: ${computerScore} . Player Score: ${humanScore}`); // log the cumulative score
+//     } 
+//     console.log("Game over!"); // let the user know the game has ended
+//     if (humanScore == computerScore) { // if it was a draw…
+//         console.log("It was a draw"); // … let the user know
+//     } else { // else tell them who the winner was
+//         humanScore > computerScore ? console.log("You win 😊") : console.log("Computer wins 😢")
+//     }
+// }
+
+// playGame()
